475bbca7702bb9e657d697c1d88b97ed953f2573
AdelWang <83192059+AdelWang@users.noreply.github.com>
2022-04-13 21:09:19 +0800

    Update label_smoother.py

--- label_smoother.py
+++ label_smoother.py
@@ -20,6 +20,6 @@ def compute_loss(self, model, inputs, classifier=None, model_extra=None, return_
         smoother_labels = F.one_hot(labels, num_classes=model.config.num_labels).float()
         for index in range(len(smoother_labels)):
             if smoother_labels[index,1] == 1:
-                smoother_labels[index] += 0.45 * overlap[index] / model.config.num_labels
-                smoother_labels[index,1] -= 0.45 * overlap[index] * (1 + 1/model.config.num_labels)
+                smoother_labels[index] += 0.45 * overlap[index] / (model.config.num_labels - 1)
+                smoother_labels[index,1] -= 0.45 * overlap[index] * (1 + 1/(model.config.num_labels - 1))
         outputs = model(**inputs,labels=smoother_labels)
